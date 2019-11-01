818653b2a799ad2d678ed972671782f684e9d0bd
Alexander Olshanskiy <sander2411ukr@gmail.com>
2019-11-01 15:59:40 +0200

    fix debounce

--- js/gallery-filtering.js
+++ js/gallery-filtering.js
@@ -5,12 +5,12 @@
   var buttonsControl = window.gallery.filtersContainer.querySelectorAll('.img-filters__button');
   var photos = [];
   var filteredPhotos;
-  var Filter = {
-    'default': window.debounce(function () {
+  var filterMap = {
+    'default': function () {
       filteredPhotos = photos;
       window.gallery.renderPhotos(filteredPhotos);
-    }),
-    'filter-random': window.debounce(function () {
+    },
+    'filter-random': function () {
       filteredPhotos = photos
         .slice()
         .sort(function () {
@@ -18,32 +18,38 @@
         })
         .slice(0, LENGTH);
       window.gallery.renderPhotos(filteredPhotos);
-    }),
-    'filter-discussed': window.debounce(function () {
+    },
+    'filter-discussed': function () {
       filteredPhotos = photos
         .slice()
         .sort(function (a, b) {
           return b.comments.length - a.comments.length;
         });
       window.gallery.renderPhotos(filteredPhotos);
-    })
+    }
   };
 
-  window.gallery.filtersContainer.addEventListener('click', function (evt) {
+  var debounceFilter = window.debounce(function (filter) {
+    var useFilter = filterMap[filter] || filterMap['default'];
+    useFilter();
+  });
+
+  var onFilterChange = function (evt) {
     var target = evt.target;
     if (target.classList.contains('img-filters__button')) {
       buttonsControl.forEach(function (btn) {
         btn.classList.remove('img-filters__button--active');
       });
       target.classList.add('img-filters__button--active');
-      var useFilter = Filter[target.id] || Filter['default'];
-      useFilter();
+      debounceFilter(target.id);
     }
-  });
+  };
+
+  window.gallery.filtersContainer.addEventListener('click', onFilterChange);
 
   var successHandler = function (photosData) {
     photos = photosData;
-    Filter['default']();
+    filterMap['default']();
   };
 
   var errorHandler = function (errorMessage) {
